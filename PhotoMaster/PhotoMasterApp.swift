28ba555fd65ae9607019966cb2af805e730ae789
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-07-20 13:54:35 +0900

    Initial Commit

---
+++ PhotoMaster/PhotoMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMasterApp.swift
+//  PhotoMaster
+//
+//  Created by A S on 2025/07/20.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoMasterTests/PhotoMasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMasterTests.swift
+//  PhotoMasterTests
+//
+//  Created by A S on 2025/07/20.
+//
+
+import Testing
+@testable import PhotoMaster
+
+struct PhotoMasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
